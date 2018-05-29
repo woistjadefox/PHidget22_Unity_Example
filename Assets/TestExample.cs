914c389e9a1f10e66fa2c7316047c18976af2f3f
Goran Saric <goran.saric@gmail.com>
2018-05-29 17:07:49 +0200

    initial commit

---
+++ Assets/TestExample.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using Phidget22;
+using Phidget22.Events;
+
+public class TestExample : MonoBehaviour {
+
+    private RCServo servo = null;
+
+    private void Start () {
+
+        servo = new RCServo();
+        servo.Open();
+        servo.Attach += OnAttach;
+    }
+
+    void OnAttach(object sender, Phidget22.Events.AttachEventArgs e) {
+
+        try {
+
+            servo.TargetPosition = servo.MaxPosition;
+            servo.Engaged = true;
+
+        } catch (PhidgetException ex) {
+            Debug.Log("Error setting initial data: " + ex.Message);
+        }
+
+    }
+
+    private void CloseConnection() {
+
+        if(servo != null) {
+            servo.Engaged = false;
+            servo.Close();
+            servo = null;
+        }
+    }
+
+    private void OnDestroy() {
+        CloseConnection();
+    }
+
+    private void OnApplicationQuit() {
+        CloseConnection();
+    }
+}
