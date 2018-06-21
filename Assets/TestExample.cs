7e7af1bb6e35e02dd04442d04b1b94a15f080b02
Goran Saric <goran.saric@gmail.com>
2018-06-21 11:11:19 +0200

    - added distance sensor example

--- Assets/TestExample.cs
+++ Assets/TestExample.cs
@@ -5,15 +5,21 @@ using Phidget22.Events;
 public class TestExample : MonoBehaviour {
 
     private RCServo servo = null;
+    private DistanceSensor sensorDistance = null;
+    private float currentSensorDistance = 0f;
 
     private void Start () {
 
         servo = new RCServo();
         servo.Open();
-        servo.Attach += OnAttach;
+        servo.Attach += OnAttachServo;
+
+        sensorDistance = new DistanceSensor();
+        sensorDistance.Open();
+        sensorDistance.Attach += OnAttachDistanceSensor;
     }
 
-    void OnAttach(object sender, Phidget22.Events.AttachEventArgs e) {
+    void OnAttachServo(object sender, Phidget22.Events.AttachEventArgs e) {
 
         try {
 
@@ -26,12 +32,31 @@ public class TestExample : MonoBehaviour {
 
     }
 
+    void OnAttachDistanceSensor(object sender, Phidget22.Events.AttachEventArgs e) {
+
+        try {
+            sensorDistance.SonarQuietMode = true;
+
+        } catch (PhidgetException ex) {
+            Debug.Log("Error setting initial data: " + ex.Message);
+        }
+
+    }
+
+    void OnDistanceChange() {
+        
+    }
+
     private void CloseConnection() {
 
         if(servo != null) {
-            servo.Engaged = false;
             servo.Close();
             servo = null;
+        }
+
+        if(sensorDistance != null) {
+            sensorDistance.Close();
+            sensorDistance = null;
         }
     }
 
@@ -42,4 +67,18 @@ public class TestExample : MonoBehaviour {
     private void OnApplicationQuit() {
         CloseConnection();
     }
+
+	private void Update() {
+		
+        if(sensorDistance != null && sensorDistance.Attached) {
+            try {
+                currentSensorDistance = sensorDistance.Distance;
+            } catch (PhidgetException ex){
+                Debug.Log("Error getting distance: " + ex.Message);
+            }
+        }
+
+
+        Debug.Log(currentSensorDistance);
+	}
 }
